a06b73553777c04a531c2f5974fdc584bcaf6391
Build With Ismail / <86028364+BuildWithIsmail@users.noreply.github.com>
2026-05-14 11:55:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 const textElement = document.getElementById('typewriter');
-const words = ["Developer", "Ui/Ux Designer", "Graphic Designer", "Motion Designer", "I can be anything!"];
+const words = ["Developer", "Ui/Ux Designer", "Graphic Designer", "Motion Designer", "Anything"];
 let wordIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
